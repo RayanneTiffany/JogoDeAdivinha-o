2f5a7032bbd1f60d455c2c6e202763475a5fc53e
RayanneTiffany <rayannetiffany.rm@gmail.com>
2024-12-04 13:26:30 -0300

    primeiro comit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Adivinhe o número");
+let numeroSecreto = 30;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (chute == numeroSecreto) {
+    console.log ( "você descobriu!");
+}
