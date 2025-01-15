0821186d10575a4da95b5aea8beb0643449f01d6
RayanneTiffany <rayannetiffany.rm@gmail.com>
2025-01-15 09:18:03 -0300

    atualização no app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-alert("Adivinhe o número");
-let numeroSecreto = 30;
-let chute = prompt("Escolha um número entre 1 e 30");
-
-if (chute == numeroSecreto) {
-    console.log ( "você descobriu!");
-}
